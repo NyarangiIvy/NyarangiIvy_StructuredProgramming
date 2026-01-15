78a05c2ca66a9c279f1272f71faf8f6f149a8a95
NyarangiIvy <nyarangiivy26@gmail.com>
2026-01-16 00:04:18 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[6][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16},
-        {17, 18, 19, 20},
-        {21, 22, 23, 24}
-    };
-    int i, j;
+    int arr[101];
+    int n, i, val, pos;
 
-    printf("Traversing the 2D array:\n");
-    for (i = 0; i < 6; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("%d\t", matrix[i][j]);
-        }
-        printf("\n");
+    printf("Input the size of array : ");
+    scanf("%d", &n);
+
+    printf("Input %d elements in the array :\n", n);
+    for (i = 0; i < n; i++) {
+        printf("element - %d : ", i);
+        scanf("%d", &arr[i]);
     }
+
+    printf("Input the value to be inserted : ");
+    scanf("%d", &val);
+    printf("Input the Position, where the value to be inserted :");
+    scanf("%d", &pos);
+
+    printf("The current list of the array : ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = val;
+    n++;
+
+    printf("\nAfter Insert the element the new list is : ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
     return 0;
 }
